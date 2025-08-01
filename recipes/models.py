5b75b0e9d2f33cff29a05fcff0f7a01e914c5b2e
brenozein <zeinbreno@gmail.com>
2025-07-31 21:46:34 -0300

    atualização foto

--- recipes/models.py
+++ recipes/models.py
@@ -8,7 +8,14 @@ class Recipe(models.Model):
     instructions = models.TextField()
     cooking_time = models.IntegerField(help_text="Em minutos")
 
+    image = models.ImageField(upload_to='recipes/', blank=True, null=True)
+
     def __str__(self):
-     return self.title
+        return self.title
+    class Meta:
+        verbose_name_plural = "Recipes"
+
+
+
 
 # Create your models here.
